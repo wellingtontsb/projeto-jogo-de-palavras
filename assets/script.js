a170f2f9804d4b27205288625147e6e7067a661c
Wellington Silva <wellingtontsb@gmail.com>
2024-12-03 23:07:03 -0300

    spliting javacript code in functions

--- assets/script.js
+++ assets/script.js
@@ -20,74 +20,84 @@ function addFields() {
 	}	
 }
 addFields();
-
 let rList = document.querySelectorAll('.list input');
+
+function verifyCentralLetter(string){
+	//transforma a palavra digitada em array
+	string = string.split('');
+
+	//verifica se palavra possui a letra obrigatoria
+	let resp = string.find((value)=>{
+		if (value == 'o' || value == 'O') {
+			return true;
+		}
+	});
+	if (!resp) {
+		console.log('Palavra não tem a letra central');
+	}
+}
+
+function verifyInvalidLetters(string) {
+	//transforma a palavra digitada em array
+	string = string.split('');
+
+	//verifica se palavra possui letras invalidas
+	string.forEach((item)=>{
+		let count = 0;
+		for(let i = 0; i<letras.length; i++){
+			console.log(item, letras[i]);
+			if (item == letras[i]) {
+				count++;
+				return;
+			}
+
+			if (i == (letras.length - 1) && count == 0) {
+				console.log('Palavra contém letras inválidas');
+				return false;
+			}
+		}
+	});
+}
+
+function verifyWordsList(string) {
+	//verifica se palavra esta na listagem
+	let list = palavras.findIndex((item)=>{
+		if (item == string) {
+			return true;
+		}
+	});
+
+	//adiona a palavra na sua posicao na lista caso verdade
+	if (list >= 0) {
+		console.log('Palavra está na lista');
+
+		rList.forEach((item)=>{
+			if (item.getAttribute('data-position') == list) {
+				item.classList.add('correct');
+				item.value = string;
+				return;
+			}
+			console.log('entrou');
+		});
+
+	}else{
+		console.log('Palavra não está na lista');
+	}
+}
 
 str.addEventListener('keyup', (e)=>{
 
 	if (e.key === 'Enter') {
-		let s = str.value;
+		let newStr = str.value;
 		str.value = '';
 
 		//verifica se palavra tem o minimo de letras possivel
-		if (s.length <= 3) {
+		if (newStr.length <= 3) {
 			console.log('Palavra não possui número mínimo de letras');
 		}else{
-			//transforma a palavra digitada em array
-			let newStr = s.split('');
-
-			//verifica se palavra possui a letra obrigatoria
-			let resp = newStr.find((value)=>{
-				if (value == 'o' || value == 'O') {
-					return true;
-				}
-			});
-			if (resp) {
-				console.log('Passou');
-			}else{
-				console.log('Palavra não tem a letra central');
-			}
-
-			//verifica se palavra possui letras invalidas
-			newStr.forEach((item)=>{
-				let count = 0;
-				for(let i = 0; i<letras.length; i++){
-					console.log(item, letras[i]);
-					if (item == letras[i]) {
-						count++;
-						return;
-					}
-
-					if (i == (letras.length - 1) && count == 0) {
-						console.log('Palavra contém letras inválidas');
-						return false;
-					}
-				}
-			});
-		}
-
-		//verifica se palavra esta na listagem
-		let list = palavras.findIndex((item)=>{
-			if (item == s) {
-				return true;
-			}
-		});
-
-		//adiona a palavra na sua posicao na lista caso verdade
-		if (list >= 0) {
-			console.log('Palavra está na lista');
-
-			rList.forEach((item)=>{
-				if (item.getAttribute('data-position') == list) {
-					item.classList.add('correct');
-					item.value = s;
-					return;
-				}
-				console.log('entrou');
-			});
-
-		}else{
-			console.log('Palavra não está na lista');
+			verifyCentralLetter(newStr);
+			verifyInvalidLetters(newStr);
+			verifyWordsList(newStr);
 		}
 	}
 });
